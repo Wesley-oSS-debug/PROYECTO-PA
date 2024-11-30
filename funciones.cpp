a762b759b57b5fc29db0d148045ec6d6444c7f9b
Oscar <oticonaf@unjbg.edu.pe>
2024-11-30 14:28:49 -0500

    Se agrego la funcionalidad crear cuenta de 3. Realizar un operacion

--- funciones.cpp
+++ funciones.cpp
@@ -4,12 +4,16 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-//cantidad de clientes
+
+void depositar(Cliente[],int);
+void crearCuenta(Cliente[],int);
+
+//Cantidad de clientes
 int nClientes() {
 	ifstream archivo;
 	string linea;
 	int n=0;
-	
+	//Calcular lineas del archivo
 	archivo.open("clientes.txt");
 	while (getline(archivo,linea)) {
 		n++;
@@ -18,6 +22,33 @@ int nClientes() {
 	
 	return n;
 }
+
+//Cargar datos de Clientes / Cuentas del txt
+void cargar(Cliente obj[],int n) {
+	ifstream archivo;
+	ifstream archivo2;
+	
+	archivo2.open("cuentasB.txt");
+	archivo.open("clientes.txt");
+	for (int i=0;i<n;i++) {
+		obj[i].cargarCliente(archivo,archivo2);
+	}
+	archivo.close();
+	archivo2.close();
+}
+
+//Actualizar datos de los archivos txt
+void actualizarDatos(Cliente obj[],int n){
+	//eliminar datos
+	remove("cuentasB.txt");
+	remove("clientes.txt");
+	//guardar datos actualizados
+	for (int i=0;i<n;i++) {
+		obj[i].guardarCliente();
+	}
+}
+
+//1. Registrar cliente
 void registrarClientes(Cliente obj[],int &n) {
 	system("cls");
 	obj[n].asignarCliente();
@@ -26,24 +57,8 @@ void registrarClientes(Cliente obj[],int &n) {
 	cout<<"numero cliente: "<<n<<endl;
 	system("pause");
 }
-//cargar clientes del txt
-void cargar(Cliente obj[],int n) {
-	ifstream archivo;
-	ifstream archivo2;
-	//cargar Clientes / Cuentas
-	archivo2.open("cuentasB.txt");
-	archivo.open("clientes.txt");
 
-	for (int i=0;i<n;i++) {
-		obj[i].cargarCliente(archivo,archivo2);
-	}
-	
-	archivo.close();
-	archivo2.close();
-
-	
-}
-
+//2. Listar Clientes
 void listarClientes(Cliente obj[],int n) {
 	system("cls");
 
@@ -55,6 +70,47 @@ void listarClientes(Cliente obj[],int n) {
 	system("pause");
 }
 
+//3. Realizar una operacion
+void operaciones(Cliente obj[],int n) {
+	int op;
+	cout<<"OPERACIONES"<<endl
+		<<"1. Crear cuenta Bancaria"<<endl
+		<<"2. Depositar dinero a una cuenta"<<endl
+		<<"3. Retirar dinero de una cuenta"<<endl
+		<<"4. Volver al menu"<<endl
+		<<"Ingrese una opcion: "; cin>>op;
+	switch (op) {
+		case 1:
+			crearCuenta(obj,n);
+			break;
+		case 2:
+			depositar(obj,n);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			cout<<"Ingrese una opcion valida"<<endl;
+	}
+}
+
+//3.1 Crear cuenta bancaria
+void crearCuenta(Cliente obj[],int n) {
+	int selec,indice;
+	cout<<"Clientes:\n"<<endl;
+	for (int i=0;i<n;i++) {
+		cout<<"Cliente ["<<i+1<<"]:\n"<<endl;
+		obj[i].mostrarCliente();
+	}
+	cout<<"Seleccione un cliente: "; cin>>selec;
+	indice=selec-1;
+	cout<<"Cliente seleccionado"<<endl;
+	obj[indice].asignarCuenta();
+	actualizarDatos(obj,n);
+}
+
+//3.2 Depositar dinero a una cuenta
 void depositar(Cliente obj[],int n) {
 	int selec,indice;
 	cout<<"Clientes:\n"<<endl;
@@ -66,33 +122,5 @@ void depositar(Cliente obj[],int n) {
 	indice=selec-1;
 	cout<<"Cliente seleccionado"<<endl;
 	obj[indice].asignarDinero();
-	
-	//eliminar datos
-	remove("cuentasB.txt");
-	remove("clientes.txt");
-	//guardar datos actualizados
-	for (int i=0;i<n;i++) {
-		obj[i].guardarCliente();
-	}
-	
-}
-
-void operaciones(Cliente obj[],int n) {
-	int op;
-	cout<<"OPERACIONES"<<endl
-		<<"1. Depositar dinero a una cuenta"<<endl
-		<<"2. Retirar dinero de una cuenta"<<endl
-		<<"3. Volver al menu"<<endl
-		<<"Ingrese una opcion: "; cin>>op;
-	switch (op) {
-		case 1:
-			depositar(obj,n);
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		default:
-			cout<<"Ingrese una opcion valida"<<endl;
-	}
+	actualizarDatos(obj,n);
 }
